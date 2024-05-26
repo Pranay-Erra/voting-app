9f799d840455d943b687177a4f62943b5efd60a7
[Pranay-Erra] <[pranayerra2003@gmail.com]>
2024-05-26 12:58:48 +0530

    elastic email undergoing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Admin_voter from './components/admin/adminVoter';
 import Admin_candidate from './components/admin/adminCandidate';
 import Admin_home from './components/admin/adminHome';
 import OTPComponent from './components/OTPComponent';
+import EmailForm from './components/emailOTP';
 
 function App() {
   
@@ -27,6 +28,7 @@ function App() {
         <Route path='/admin-candidates' element={<Admin_candidate/>}/>
         <Route path='/admin-home' element={<Admin_home/>}/>
         <Route path='/otp-entry' element={<OTPComponent/>}/>
+        <Route path='/email' element={<EmailForm/>}/>
       </Routes>
     </BrowserRouter>
   );
